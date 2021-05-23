bda4ef41c9ab8268333e9d1e24dca1b5ba174f43
Simon <simonjsampson@gmail.com>
2021-05-23 15:54:49 +0100

    Completed all routes to manage user testData

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -8,14 +8,21 @@ const router: Router = express.Router();
 // @route GET api/test
 // @desc Get a user's test data
 // @access Private
-router.get('/', auth, async (req: Request, res: Response) => {
+router.get('/:userid', auth, async (req: Request, res: Response) => {
+    const userIdParams = req.params.userid;
+    const userIdToken = req.user?._id;
+
+    if (userIdParams !== userIdToken?.toString()) {
+        return res.status(401).json({ msg: 'User not authorized' });
+    }
+
     try {
         console.log('GET api/test hit');
-        const test = await Test.findOne({ user: req.user?._id });
-        if (!test) {
+        const testRecord = await Test.findOne({ user: userIdParams });
+        if (!testRecord) {
             return res.status(404).json({ msg: 'Test data not found' });
         }
-        return res.json(test);
+        return res.json(testRecord);
     } catch (err) {
         console.error(err.message);
         return res.status(500).send('Server error');
@@ -25,29 +32,101 @@ router.get('/', auth, async (req: Request, res: Response) => {
 // @route POST api/test
 // @desc Add some test data to a user
 // @access Private
-router.post('/', auth, check('testVar', 'testVar is required').not().isEmpty(), async (req: Request, res: Response) => {
-    console.log('POST api/test hit');
-    const errors: Result<ValidationError> = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+router.post(
+    '/:userid',
+    auth,
+    check('testVar', 'testVar is required').not().isEmpty(),
+    async (req: Request, res: Response) => {
+        console.log('POST api/test hit');
+
+        const userIdParams = req.params.userid;
+        const userIdToken = req.user?._id;
+
+        if (userIdParams !== userIdToken?.toString()) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        const errors: Result<ValidationError> = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { testVar } = req.body;
+        const testData = { testVar };
+
+        try {
+            let foundTestData = await Test.findOne({ user: userIdParams });
+            if (!foundTestData) {
+                const newTestDataRecord: ItestData = new Test({ user: userIdParams, testData: [testData] });
+                await newTestDataRecord.save();
+                return res.status(200).json({ msg: 'Test data added' });
+            } else {
+                await Test.findOneAndUpdate({ user: userIdParams }, { $push: { testData: testData } });
+                return res.status(200).json({ msg: 'Test data added' });
+            }
+        } catch (err) {
+            console.log(err.message);
+            return res.status(500).send('Server error');
+        }
+    }
+);
+
+// @route DELETE api/test/:userid/:testid
+// @desc Delete some test data from a user
+// @access Private
+router.delete('/:userid/:testid', auth, async (req: Request, res: Response) => {
+    console.log('DELETE api/test/:userid/:testid hit');
+
+    const userIdParams = req.params.userid;
+    const userIdToken = req.user?._id;
+    const testDataId = req.params.testid;
+
+    if (userIdParams !== userIdToken?.toString()) {
+        return res.status(401).json({ msg: 'User not authorized' });
     }
 
-    const { testVar } = req.body;
-    const id = req.user?._id;
-    const testData = { testVar };
-
     try {
-        let foundTestData = await Test.findOne({ user: id });
-        if (!foundTestData) {
-            const newTestDataRecord: ItestData = new Test({ user: id, testData: [testData] });
-            await newTestDataRecord.save();
-            return res.status(200).send();
+        const foundTestRecord = await Test.findOne({ user: userIdParams, 'testData._id': testDataId });
+        if (foundTestRecord) {
+            console.log(foundTestRecord);
+            await Test.findOneAndUpdate({ user: userIdParams }, { $pull: { testData: { _id: testDataId } } });
+            return res.status(200).json({ msg: 'Test data removed' });
         } else {
-            await Test.findOneAndUpdate({ user: id }, { $push: { testData: testData } });
-            return res.status(200).send();
+            return res.status(404).json({ msg: 'Test data not found' });
         }
     } catch (err) {
-        console.log(err.message);
+        if (err.name === 'CastError') {
+            return res.status(404).json({ msg: 'Invalid id' });
+        }
+        return res.status(500).send('Server error');
+    }
+});
+
+// @route DELETE api/test/:userid/
+// @desc Delete all test data from a user
+// @access Private
+router.delete('/:userid', auth, async (req: Request, res: Response) => {
+    console.log('DELETE api/test/:userid hit');
+
+    const userIdParams = req.params.userid;
+    const userIdToken = req.user?._id;
+
+    if (userIdParams !== userIdToken?.toString()) {
+        return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    try {
+        const foundTestRecord = await Test.findOne({ user: userIdParams });
+        if (foundTestRecord) {
+            await Test.deleteOne({ user: userIdParams });
+            return res.status(200).json({ msg: 'Test data removed' });
+        } else {
+            return res.status(404).json({ msg: 'Test data not found' });
+        }
+    } catch (err) {
+        if (err.name === 'CastError') {
+            return res.status(404).json({ msg: 'Invalid id' });
+        }
         return res.status(500).send('Server error');
     }
 });
